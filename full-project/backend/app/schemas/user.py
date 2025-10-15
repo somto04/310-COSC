a3210a0c5f1a356a240f35fe439df74dadf9ffb9
somto04 <s.azubuike1@lancaster.ac.uk>
2025-10-15 16:35:44 -0700

    created user schema

---
+++ full-project/backend/app/schemas/user.py
@@ -0,0 +1,27 @@
+class user():
+    userId: int
+    firstname: str
+    lastname: str
+    age: int
+    email: str
+    username: str
+    pw: str
+    role: str
+
+class userCreate():
+    firstname: str
+    lastname: str
+    age: int
+    email: str
+    username: str
+    pw: str
+    role: str
+
+class userUpdate():
+    firstname: str
+    lastname: str
+    age: int
+    email: str
+    username: str
+    pw: str
+    role: str

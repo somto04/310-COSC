e5c46d434ee4be2e30eab96b6baebd662735436c
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-10-16 11:16:34 -0700

    added user class under model folder

---
+++ full-project/backend/app/model/user.py
@@ -0,0 +1,12 @@
+class user:
+    penaltiesCount = 0
+
+    def __init__(self, userId, name, age, email, username, password):
+        self.userId = userId
+        self.name = name
+        self.age = age
+        self.email = email
+        self.username = username
+        self.password = password
+
+    

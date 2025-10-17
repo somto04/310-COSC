25d3de658b56ae015e5d9bd75566f8a41ea1d99d
somto04 <s.azubuike1@lancaster.ac.uk>
2025-10-16 22:15:56 -0700

    completed the services for user and edited the user repo file to be a .py

---
+++ full-project/backend/app/services/userService.py
@@ -0,0 +1,57 @@
+import uuid
+from fastapi import HTTPException
+from schemas.user import User, UserCreate, UserUpdate
+from repos.userRepo import load_all, save_all
+
+def listUsers() -> List[User]:
+    return [User(**it) for it in load_all()]
+
+def createUser(payload: UserCreate) -> User:
+    users = load_all()
+    newId = str(uuid.uuid4())
+    if any(it.get("id") == newId for it in users):
+        raise HTTPException(status_code=409, detail="ID collision; retry.")
+    newUser = User(id=newId, 
+                   firstname = payload.firstname.strip(), 
+                   lastname = payload.lastname.strip(), 
+                   age = payload.age.strip(),
+                   email = payload.email.strip(),
+                   username = payload.username.strip(),
+                   pw = payload.pw.strip(),
+                   role = payload.role)
+    users.append(newUser.dict())
+    save_all(users)
+    return newUser
+
+def getUserById(userId: str) -> User:
+    users = load_all()
+    for it in users:
+        if it.get("id") == userId:
+            return User(**it)
+    raise HTTPException(status_code=404, detail=f"User '{userId}' not found")
+
+def updateUser(userId: str, payload: UserUpdate) -> User:
+    users = load_all()
+    for idx, it in enumerate(users):
+        if it.get("id") == userId:
+            updated = User(
+                id=userId,
+                firstname = payload.firstname.strip(), 
+                lastname = payload.lastname.strip(), 
+                age = payload.age.strip(),
+                email = payload.email.strip(),
+                username = payload.username.strip(),
+                pw = payload.pw.strip(),
+                role = payload.role,
+            )
+            users[idx] = updated.dict()
+            save_all(users)
+            return updated
+    raise HTTPException(status_code=404, detail=f"User '{userId}' not found")
+
+def deleteUser(userId: str) -> None:
+    users = load_all()
+    newUsers = [it for it in users if it.get("id") != userId]
+    if len(newUsers) == len(users):
+        raise HTTPException(status_code=404, detail=f"User '{userId}' not found")
+    save_all(newUsers)

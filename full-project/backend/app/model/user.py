263cc9d07c82ccc490c890763d579680719de882
somto04 <s.azubuike1@lancaster.ac.uk>
2025-10-29 15:12:19 -0700

    added the role attribute to the model and test

--- full-project/backend/app/model/user.py
+++ full-project/backend/app/model/user.py
@@ -1,13 +1,14 @@
 class user:
     penaltiesCount = 0
 
-    def __init__(self, userId, name, age, email, username, password):
+    def __init__(self, userId, name, age, email, username, password, role):
         self.userId = userId
         self.name = name
         self.age = age
         self.email = email
         self.username = username
         self.password = password
+        self.role = role
 
     # user getters
     def get_userId(self):
@@ -30,6 +31,9 @@ class user:
     
     def get_penaltiesCount(self):
         return self.penaltiesCount
+    
+    def get_role(self):
+        return self.role
     
     # user setters
     
@@ -54,4 +58,7 @@ class user:
     def set_penaltiesCount(self, penaltiesCount):
         self.penaltiesCount = penaltiesCount
 
+    def set_role(self, role):
+        self.role = role
+    
 

91cff5a06960a1a7335e5762d031c42c492cebe0
somto04 <s.azubuike1@lancaster.ac.uk>
2025-11-04 11:26:22 -0800

    refactored review route

--- full-project/backend/app/routers/reviewRoute.py
+++ full-project/backend/app/routers/reviewRoute.py
@@ -15,7 +15,7 @@ def searchReview(q: str = "", limit: int = 50, offset: int = 0):
 def getReviews():
     return listReviews()
 
-@router.post("", response_model=Review, status_code=201)
+@router.post("", response_model=Review, currentUser: dict = Depends(getCurrentUser), status_code=201)
 def postReview(payload: ReviewCreate):
     return createReview(payload)
 
@@ -28,13 +28,17 @@ def putReview(reviewId: str, payload: ReviewUpdate):
     return updateReview(reviewId, payload)
 
 @router.delete("/{reviewId}", status_code=status.HTTP_204_NO_CONTENT)
-# gets the current user from auth
 def removeReview(reviewId: str, currentUser: dict = Depends(getCurrentUser)): 
     review = getReviewById(reviewId)
+    validateReview(review)
+    validateAdminOrReveiwOwner(currentUser, review)
+    deleteReview(reviewId)
+    return None
+
+def validateReview(review):
     if not review:
         raise HTTPException(status_code=404, detail="Review not found")
     
+def validateAdminOrReveiwOwner(currentUser, review):
     if currentUser["role"] != "admin" and review["username"] != currentUser["username"]:
         raise HTTPException(status_code=403, detail="not authorised to delete this review")
-    deleteReview(reviewId)
-    return None

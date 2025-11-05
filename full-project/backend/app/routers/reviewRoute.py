cee799033105bec0795b8d116a0b58be2cfbe4d8
Camilla Cloutier <camillacloutier45@gmail.com>
2025-11-04 17:34:33 -0800

    finished reviewRoute test file

--- full-project/backend/app/routers/reviewRoute.py
+++ full-project/backend/app/routers/reviewRoute.py
@@ -1,7 +1,9 @@
 from typing import List
 from fastapi import APIRouter, status, Depends, HTTPException
 from ..schemas.review import Review, ReviewCreate, ReviewUpdate
-from ..services.reviewService import listReviews, createReview, deleteReview, updateReview, getReviewById
+from ..services.reviewService import listReviews, createReview, deleteReview, updateReview, getReviewById, searchReviews
+from app.routers.auth import getCurrentUser  
+
 
 router = APIRouter(prefix = "/reviews", tags = ["reviews"])
 

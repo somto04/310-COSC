ee89d259c82ae7604cde8188ffe7696ca2da0edb
somto04 <s.azubuike1@lancaster.ac.uk>
2025-11-14 14:30:56 -0800

    added doc string to reply route

--- full-project/backend/app/routers/replyRoute.py
+++ full-project/backend/app/routers/replyRoute.py
@@ -6,14 +6,12 @@ from .auth import getCurrentUser
 
 router = APIRouter(prefix="/replies", tags=["replies"])
 
-# Endpoint to get all replies for a specific review
 @router.get("/{reviewId}", response_model=List[Reply])
 def getReplies(reviewId: int):
+    """ Returns all replies that match a reviewId """
     return list_replies(reviewId)
 
-# Endpoint to create a new reply
 @router.post("", response_model=Reply)
-# If you later want to restrict reply creation to logged-in users, uncomment below
-# def postReply(payload: ReplyCreate, currentUser: dict = Depends(getCurrentUser)):
-def postReply(payload: ReplyCreate):
+def postReply(payload: ReplyCreate, currentUser: dict = Depends(getCurrentUser)):
+    """ Creates a mew reply (only logged in users are able to post one)"""
     return create_reply(payload)

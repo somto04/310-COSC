d5bed8636a1613a43394cdf5e9ef3672b329affb
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-11-16 13:01:47 -0800

    made imdb rating optional in main movie model, MovieCreate and MovieUpdate to follow main model typing and aliases validation,

--- full-project/backend/app/schemas/movie.py
+++ full-project/backend/app/schemas/movie.py
@@ -6,7 +6,8 @@ from datetime import date
 class Movie(BaseModel):
     id: int = Field(validation_alias = AliasChoices("id", "movieId"))
     title: str = Field(validation_alias = AliasChoices("title", "movieName"))
-    movieIMDbRating: Decimal = Field(
+    movieIMDbRating: Optional[Decimal] = Field(
+        default = None,
         ge = 1.0, 
         le = 10.0,
         max_digits = 3,
@@ -41,11 +42,13 @@ class Movie(BaseModel):
 
 class MovieCreate(BaseModel):
     title: str = Field(validation_alias = AliasChoices("title", "movieName"))
-    movieGenres: List[str]
+    movieGenres: List[str] = Field(validation_alias = AliasChoices("movieGenre", "movieGenres"))
     directors: List[str] = Field(default_factory = list)
-    mainStars: List[str] = Field(default_factory = list)
+    mainStars: List[str] = Field(
+        default_factory = list,
+        validation_alias = AliasChoices("mainstars", "mainStars"))
     description: Optional[str] = None
-    datePublished: Optional[date] = None  # ISO date string
+    datePublished: Optional[str] = None  # ISO date string
     duration: int = Field(
         gt = 0,
         validation_alias = AliasChoices("duration", "length"))  # minutes
@@ -57,11 +60,26 @@ class MovieCreate(BaseModel):
 
 class MovieUpdate(BaseModel):
     title: Optional[str] = Field(default = None, validation_alias = AliasChoices("title", "movieName"))
-    movieIMDbRating: Optional[float] = None
-    movieGenres: Optional[List[str]] = None
-    directors: Optional[List[str]] = Field(default_factory = list)
-    mainStars: Optional[List[str]] = Field(default_factory = list)
+    movieIMDbRating: Optional[Decimal] = Field(
+        default = None,
+        ge = 1.0, 
+        le = 10.0,
+        max_digits = 3,
+        decimal_places = 1,
+        validation_alias = AliasChoices("movieIMDb", "movieIMDbRating"))
+    movieGenres: Optional[List[str]] = Field(default = None, validation_alias = AliasChoices("movieGenre", "movieGenres"))
+    directors: Optional[List[str]] = None
+    mainStars: Optional[List[str]] = Field(
+        default = None,
+        validation_alias = AliasChoices("mainstars", "mainStars"))
     description: Optional[str] = None
-    datePublished: Optional[str] = None  # ISO date string
-    duration: Optional[int] = Field(default = None, validation_alias = AliasChoices("duration", "length"))  # minutes
-    yearReleased: Optional[int] = None
+    datePublished: Optional[date] = None  # ISO date string
+    duration: Optional[int] = Field(
+        default = None,
+        gt = 0,
+        validation_alias = AliasChoices("duration", "length"))  # minutes
+    yearReleased: Optional[int] = Field(
+        default = None,
+        ge = 1888,  # The year the first film was made
+        le = 2100,
+        validation_alias = AliasChoices("yearReleased", "year"))

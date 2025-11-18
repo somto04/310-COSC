dfbf63cac60f89b4789e5475f1e57681b4dd4904
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-11-18 14:50:03 -0800

    changed loadAll to return a list of User

--- full-project/backend/app/repos/userRepo.py
+++ full-project/backend/app/repos/userRepo.py
@@ -1,16 +1,19 @@
 from typing import List, Dict, Any
 from .repo import _base_load_all, _base_save_all, DATA_DIR
+from ..schemas.user import User
 
 USER_DATA_FILE = DATA_DIR / "users.json"
 
-def loadAll() -> List[Dict[str, Any]]:
+def loadAll() -> List[User]:
     """
     Load all users from the users data file.
 
     Returns:
-        List[Dict[str, Any]]: A list of user items.
+        List[User]: A list of users.
     """
-    return _base_load_all(USER_DATA_FILE)
+
+    user_dicts = _base_load_all(USER_DATA_FILE)
+    return [User(**user) for user in user_dicts]
 
 def saveAll(items: List[Dict[str, Any]]) -> None:
     """

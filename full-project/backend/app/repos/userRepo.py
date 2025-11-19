467ffb3cb1592e2b9e4d29ce01fbd9daa1624d09
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-11-18 16:02:02 -0800

    implemented load cache function for efficiency

--- full-project/backend/app/repos/userRepo.py
+++ full-project/backend/app/repos/userRepo.py
@@ -2,8 +2,22 @@ from typing import List, Dict, Any
 from .repo import _base_load_all, _base_save_all, DATA_DIR
 from ..schemas.user import User
 
-USER_DATA_PATH = DATA_DIR / "users.json"
+_USER_DATA_PATH = DATA_DIR / "users.json"
+_USER_CACHE: List[User] | None = None
 
+def _load_cache() -> List[User]:
+    """
+    Load users from the data file into a cache.
+
+    Helper to load users efficiently.
+    Returns:
+        List[User]: A list of users.
+    """
+    global _USER_CACHE
+    if _USER_CACHE is None:
+        user_dicts = _base_load_all(_USER_DATA_PATH)
+        _USER_CACHE = [User(**user) for user in user_dicts]
+    return _USER_CACHE
 
 def loadAll() -> List[User]:
     """
@@ -12,9 +26,7 @@ def loadAll() -> List[User]:
     Returns:
         List[User]: A list of users.
     """
-
-    user_dicts = _base_load_all(USER_DATA_PATH)
-    return [User(**user) for user in user_dicts]
+    return _load_cache()
 
 
 def saveAll(users: List[User]):
@@ -25,7 +37,7 @@ def saveAll(users: List[User]):
         users (List[User]): A list of users to save.
     """
     user_dicts = [user.model_dump() for user in users]
-    _base_save_all(USER_DATA_PATH, user_dicts)
+    _base_save_all(_USER_DATA_PATH, user_dicts)
 
 
 __all__ = ["loadAll", "saveAll"]

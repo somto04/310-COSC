56c5ede4e0357bcff5ee4c001736629751d91fdf
somto04 <s.azubuike1@lancaster.ac.uk>
2025-11-19 15:40:01 -0800

    made all ratings decimals instead of strings

--- full-project/backend/app/repos/tests/test_reviewRepo.py
+++ full-project/backend/app/repos/tests/test_reviewRepo.py
@@ -12,7 +12,7 @@ def test_review_load_uses_tmp(tmp_path, monkeypatch):
             userId= 7,
             reviewTitle= "A Tale Worth a Thousand Berries",
             reviewBody= "By the seas! This film had more heart than a feast at Baratie. Zoro nearly cried—well, almost.",
-            rating= 9,
+            rating= 9.5,
             datePosted= "2025-01-12",
             flagged= False
         ),
@@ -22,7 +22,7 @@ def test_review_load_uses_tmp(tmp_path, monkeypatch):
             userId= 3,
             reviewTitle= "Not Even the Grand Line Could Save It",
             reviewBody= "Nami said it was a treasure, but the pacing dragged worse than Usopp’s tall tales. Needed more spirit!",
-            rating= "6.0",
+            rating= 6,
             datePosted= "2025-03-22",
             flagged= True
         )
@@ -60,7 +60,7 @@ def test_review_save_and_verify_contents(tmp_path, monkeypatch):
         userId= 3,
         reviewTitle= "Not Even the Grand Line Could Save It",
         reviewBody= "Nami said it was a treasure, but the pacing dragged worse than Usopp’s tall tales. Needed more spirit!",
-        rating= "6.0",
+        rating= 6,
         datePosted= "2025-03-22",
         flagged= True
     ),
@@ -70,7 +70,7 @@ def test_review_save_and_verify_contents(tmp_path, monkeypatch):
         userId= 10,
         reviewTitle= "Sanji’s Culinary Masterpiece",
         reviewBody= "Cooked to perfection! Every scene sizzled like meat on the grill. I’d trade a barrel of cola for another viewing.",
-        rating= "8.8",
+        rating= 8.8,
         datePosted= "2025-05-04",
         flagged= False
     ),
@@ -93,7 +93,7 @@ def test_review_save_and_verify_contents(tmp_path, monkeypatch):
     assert contents == data
     assert len(contents) == 4
     assert contents[2].reviewTitle == "Sanji’s Culinary Masterpiece"
-    assert contents[3].rating == "5.7"
+    assert contents[3].rating == 5.7
     assert contents[3].reviewBody.startswith("Plot twist hit harder than Luffy’s Gatling")
     assert contents[1].flagged is True
     assert contents[0].userId == 7

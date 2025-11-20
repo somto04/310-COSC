f97ec3f9a9e37c2782b9584f74dcb898a55a348a
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-11-19 19:54:08 -0800

    fixed fake data to use correct User model

--- full-project/backend/app/utilities/tests/test_penalty.py
+++ full-project/backend/app/utilities/tests/test_penalty.py
@@ -1,25 +1,31 @@
 import pytest
 from app.utilities.penalties import incrementPenaltyForUser
 from app.schemas.user import User
+from app.schemas.role import Role
+
 
 def test_IncrementPenalty(monkeypatch):
     # fake user data
     fake_users = [
-        User(id=1, username="test", email="x", password="y", age=20, 
-             role="user",penalties=0, isBanned=False)
+        User(
+            id=1,
+            username="test",
+            email="sigma@example.com",
+            age=20,
+            role=Role.USER,
+            penalties=0,
+            isBanned=False,
+            firstName="Test",
+            lastName="User",
+            pw="hashedSigma21",
+        )
     ]
 
-    
-    monkeypatch.setattr(
-        "app.utilities.penalties.loadUsers",
-        lambda: fake_users
-    )
+    monkeypatch.setattr("app.utilities.penalties.loadUsers", lambda: fake_users)
 
-    
     saved = []
     monkeypatch.setattr(
-        "app.utilities.penalties.saveUsers",
-        lambda users: saved.extend(users)
+        "app.utilities.penalties.saveUsers", lambda users: saved.extend(users)
     )
 
     updated = incrementPenaltyForUser(1)
@@ -28,24 +34,29 @@ def test_IncrementPenalty(monkeypatch):
     assert updated.isBanned is False
     assert saved[0].penalties == 1
 
+
 def test_IncrementPenaltyBan(monkeypatch):
     # fake user data
     fake_users = [
-        User(id=2, username="test", email="x", password="y", age=20, role="user",
-             penalties=2, isBanned=False)
+        User(
+            id=2,
+            username="beta",
+            email="beta@example.com",
+            age=20,
+            role=Role.USER,
+            penalties=2,
+            isBanned=False,
+            firstName="Beta",
+            lastName="Tester",
+            pw="hashedBeta22",
+        )
     ]
 
-    
-    monkeypatch.setattr(
-        "app.utilities.penalties.loadUsers",
-        lambda: fake_users
-    )
+    monkeypatch.setattr("app.utilities.penalties.loadUsers", lambda: fake_users)
 
-    
     saved = []
     monkeypatch.setattr(
-        "app.utilities.penalties.saveUsers",
-        lambda users: saved.extend(users)
+        "app.utilities.penalties.saveUsers", lambda users: saved.extend(users)
     )
 
     updated = incrementPenaltyForUser(2)
@@ -54,4 +65,3 @@ def test_IncrementPenaltyBan(monkeypatch):
     assert updated.isBanned is True
     assert saved[0].penalties == 3
     assert saved[0].isBanned is True
-

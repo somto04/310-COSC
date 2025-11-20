585de1005f97ef94a103cdcafd78fdd1d87f1ed3
somto04 <s.azubuike1@lancaster.ac.uk>
2025-11-20 10:10:44 -0800

    updated reciew repo correctly

--- full-project/backend/app/repos/reviewRepo.py
+++ full-project/backend/app/repos/reviewRepo.py
@@ -4,13 +4,30 @@ from ..schemas.review import Review
 
 REVIEW_DATA_PATH = DATA_DIR / "reviews.json"
 _REVIEW_CACHE: List[Review] | None = None
-_NEXT_USER_ID: int | None = None
+_NEXT_REVIEW_ID: int | None = None
 
 def getMaxReviewId(reviews: List[Review]) -> int:
     """
     Return the maximum Review ID in a list of reviews, or 0 if empty.
     """
     return max((review.id for review in reviews), default=0)
+
+def getNextReviewId() -> int:
+    """
+    Get the next available review ID.
+
+    Returns:
+        int: The next review ID.
+    """
+    global _NEXT_REVIEW_ID
+    if _NEXT_REVIEW_ID is None:
+        _load_review_cache()
+
+    assert _NEXT_REVIEW_ID is not None
+
+    next_id = _NEXT_REVIEW_ID
+    _NEXT_REVIEW_ID += 1
+    return next_id
 
 
 def _load_review_cache() -> List[Review]:
@@ -21,13 +38,13 @@ def _load_review_cache() -> List[Review]:
     Returns:
         List[Review]: A list of reviews.
     """
-    global _REVIEW_CACHE, _NEXT_USER_ID
+    global _REVIEW_CACHE, _NEXT_REVIEW_ID
     if _REVIEW_CACHE is None:
         review_dicts = _base_load_all(REVIEW_DATA_PATH)
         _REVIEW_CACHE = [Review(**review) for review in review_dicts]
 
         maxId = getMaxReviewId(_REVIEW_CACHE)
-        _NEXT_USER_ID = maxId + 1
+        _NEXT_REVIEW_ID = maxId + 1
     return _REVIEW_CACHE
 
 def loadReviews() -> List[Review]:
@@ -46,13 +63,13 @@ def saveReviews(reviews: List[Review]) -> None:
     Args:
         reviews (List[Review]): A list of review items to save.
     """
-    global _REVIEW_CACHE, _NEXT_USER_ID
+    global _REVIEW_CACHE, _NEXT_REVIEW_ID
     _REVIEW_CACHE = reviews
 
     maxId = getMaxReviewId(reviews)
-    if _NEXT_USER_ID is None or _NEXT_USER_ID <= maxId:
-        _NEXT_USER_ID = maxId + 1
-        
+    if _NEXT_REVIEW_ID is None or _NEXT_REVIEW_ID <= maxId:
+        _NEXT_REVIEW_ID = maxId + 1
+
     review_dict = [review.model_dump() for review in reviews]
     _base_save_all(REVIEW_DATA_PATH, review_dict)
 

f80f1e70f679ea3d1e38ea2f6bb970dfab64c050
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-11-27 10:44:57 -0800

    renamed put and post movie routers

--- full-project/backend/app/routers/movieRoute.py
+++ full-project/backend/app/routers/movieRoute.py
@@ -65,13 +65,14 @@ def getMovie(movieId: int):
 
 # ADMIN ONLY #
 
-# @router.post("", response_model=Movie, status_code=status.HTTP_201_CREATED)
-# def postMovie(payload: MovieCreate, admin: dict = Depends(requireAdmin)):
-# return createMovie(payload)
+
+@router.post("", response_model=Movie, status_code=status.HTTP_201_CREATED)
+def addMovie(payload: MovieCreate, admin: dict = Depends(requireAdmin)):
+    return createMovie(payload)
 
 
 @router.put("/{movieId}", response_model=Movie)
-def updateMovie(
+def modifyMovieDetails(
     movieId: int, payload: MovieUpdate, admin: dict = Depends(requireAdmin)
 ):
     try:

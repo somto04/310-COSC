1080863ce42eefffeaa7a70aec719091c39c743a
Camilla Cloutier <camillacloutier45@gmail.com>
2025-11-27 11:53:50 -0800

    accepted change so that users will not all share the same list

--- full-project/backend/app/schemas/user.py
+++ full-project/backend/app/schemas/user.py
@@ -105,8 +105,8 @@ class User(BaseModel):
         default=0, validation_alias=AliasChoices("penaltyCount", "penalties")
     )
     isBanned: bool = False
-    likedReviews: Optional[list[int]] = []
-    watchlist: List[int] = []
+    likedReviews: Optional[list[int]] = Field(default_factory=list)
+    watchlist: List[int] = Field(default_factory=list)
 
 
 class UserCreate(BaseModel):

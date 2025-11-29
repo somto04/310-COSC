2746e8ebd94186cd02841ab81f9ff2cbc9cf3f4b
somto04 <s.azubuike1@lancaster.ac.uk>
2025-11-29 14:15:18 -0800

    yyayysyayayaay movies now showing ahhhhhh

--- full-project/backend/app/externalAPI/tmdbService.py
+++ full-project/backend/app/externalAPI/tmdbService.py
@@ -42,7 +42,8 @@ def getMovieDetailsById(tmdbId: int) -> TMDbMovie | None:
     )
     data = response.json()
 
-    if data.get("status_code") is not None:
+    if data.get("status_code"):
+        print("TMDB ERROR:", data)
         return None
 
     return TMDbMovie(

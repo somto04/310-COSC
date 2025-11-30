2cd09fbf396e44d4c53aa2b852b8a549a4e2d2d2
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-11-29 18:59:47 -0800

    additional exceptions

--- full-project/backend/app/services/adminService.py
+++ full-project/backend/app/services/adminService.py
@@ -1,7 +1,7 @@
 from app.schemas.user import CurrentUser, User
 from app.schemas.user import AdminUserUpdate
 from app.schemas.role import Role
-from app.services.userService import updateUser
+from app.services.userService import getUserById, updateUser
 
 
 class AdminActionError(Exception):
@@ -12,6 +12,9 @@ def grantAdmin(userId: int, currentAdmin: CurrentUser) -> User:
     """Grant admin privileges to a user."""
     if currentAdmin.id == userId:
         raise AdminActionError("You are already an admin, silly")
+    currentUser = getUserById(userId)
+    if currentUser.role == Role.ADMIN:
+        raise AdminActionError("User is already an admin")
     
     update = AdminUserUpdate(role=Role.ADMIN)
     updatedUser = updateUser(userId, update)
@@ -22,6 +25,9 @@ def revokeAdmin(userId: int, currentAdmin: CurrentUser) -> User:
     """Revoke admin privileges from a user."""
     if currentAdmin.id == userId:
         raise AdminActionError("Cannot revoke your own admin privileges")
+    currentUser = getUserById(userId)
+    if currentUser.role != Role.ADMIN:
+        raise AdminActionError("User is not an admin")
     
     update = AdminUserUpdate(role=Role.USER)
     updatedUser = updateUser(userId, update)

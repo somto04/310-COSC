5b03cb415a3d8da2d176ddde5690a02cf0f653e6
Gamu Mhere <gamu.mhere@gmail.com>
2025-12-02 18:51:03 -0800

    successfully creating an account redirects to the homepage

--- full-project/frontend/src/pages/CreateAccount.tsx
+++ full-project/frontend/src/pages/CreateAccount.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const API = import.meta.env.VITE_API_URL;
+
+const navigate = useNavigate();
 
 export default function Register() {
   const [username, setUsername] = useState("");
@@ -9,7 +12,7 @@ export default function Register() {
   const [pw, setPassword] = useState("");
   const [age, setAge] = useState("");
   const [message, setMessage] = useState("");
-
+  
   // Clean labels for each backend field
   const fieldLabels: Record<string, string> = {
     username: "Username",
@@ -91,7 +94,7 @@ export default function Register() {
         setMessage("Account created successfully!");
 
         
-        window.location.href = "/homepage";
+        navigate("/");
       })
       .catch((err) => {
         console.error("Error creating account:", err);

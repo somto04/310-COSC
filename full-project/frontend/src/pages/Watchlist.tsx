2dd11780594bad5622afd0fac406ba657d746c29
somto04 <s.azubuike1@lancaster.ac.uk>
2025-12-02 18:17:15 -0800

    added movie type

--- full-project/frontend/src/pages/Watchlist.tsx
+++ full-project/frontend/src/pages/Watchlist.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+const API = import.meta.env.VITE_API_URL;
+
 export default function WatchlistPage() {
+  type Movie = {
+    id: number;
+    tmdbId?: number;
+    title: string;
+    poster?: string | null;
+    overview?: string;
+    rating?: number;
+  };
+    
+    
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Watchlist</h1>

566ab60286754ed30b86c89ebdb3e1fd60b833c7
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-12-03 01:13:03 -0800

    login returns username for convenience

--- full-project/backend/app/routers/authRoute.py
+++ full-project/backend/app/routers/authRoute.py
@@ -117,6 +117,7 @@ def login(
         "message": "Login successful",
         "access_token": createAccessToken(username),
         "token_type": "bearer",
+        "username": user.username,
         "userId": user.id,
         "role": user.role,
         "isAdmin": user.role == Role.ADMIN,

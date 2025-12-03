b4b1ead9a294f3f44a461333e2c85864ada84468
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-12-03 01:13:31 -0800

    user name local storage functions

--- full-project/frontend/src/utils/auth.tsx
+++ full-project/frontend/src/utils/auth.tsx
@@ -12,22 +12,40 @@ export function getUserId() {
     return localStorage.getItem("userId");
 }
 
-export function setAuth(token: string, userId: string, isAdmin: boolean) {
+export function getUsername() {
+    return localStorage.getItem("username");
+}
+
+export function setAuth(token: string, userId: string, isAdmin: boolean, username: string) {
     localStorage.setItem("token", token);
     localStorage.setItem("userId", userId);
     localStorage.setItem("isAdmin", isAdmin ? "true" : "false");
+    localStorage.setItem("username", username);
 }
 
 export function clearAuth() {
     localStorage.removeItem("token");
     localStorage.removeItem("isAdmin");
     localStorage.removeItem("userId");
+    localStorage.removeItem("username");
 }
 
 export function requireAuth(element: React.ReactElement) {
     const token = getToken();
 
     if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return element;
+}
+
+export function requireAdmin(element: React.ReactElement) {
+    const auth = requireAuth(element);
+    
+    if (auth !== element) return auth;  // not logged in then redirect
+
+    if (!getIsAdmin()) {
         return <Navigate to="/login" replace />;
     }
 

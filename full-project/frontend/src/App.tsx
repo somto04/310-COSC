36778493a4241e393cbe4ceb436bf05bb162beb3
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-12-02 17:09:44 -0800

    test page for testing convenience, did i say test enough yet

--- full-project/frontend/src/App.tsx
+++ full-project/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
 import './App.css'
+import TestPage from './pages/testPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         {/* <Route path="/" element={<Homepage />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/test" element={<TestPage />} />
       </Routes>
     </>
   )

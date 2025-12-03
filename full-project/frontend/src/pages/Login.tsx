9561801c615138b41d799da9b3552f8a4f49fcb8
zaladmander <159731834+zaladmander@users.noreply.github.com>
2025-12-03 11:17:28 -0800

    unused import

--- full-project/frontend/src/pages/Login.tsx
+++ full-project/frontend/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { setAuth } from "../utils/auth";
